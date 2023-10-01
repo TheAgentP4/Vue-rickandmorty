f081d985e3f5fc7d64587aa24d2635fcce95f7ef
Diego Medi <diegomr090@gmail.com>
2023-09-30 18:05:56 -0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+        <nav class=" text-center m-5 text-3xl">
+            <a href="">Personajes </a>
+            <a href="./Location.vue">   Ubicación</a>
+        </nav>
+        
+        <hr class=" border-1 border-green">
+        <div class="grid grid-cols-4 gap-5 p-10">
+            
+            <section v-for="value in character " class="">
+
+                <div class="content-center border-2 border-green p-10 ">
+
+                    <div class=" flex justify-center">
+                        <img class="  h-[250px] w-[250px] " :src=" `${value.image }`" alt="">
+                    </div>
+                    <hr  class="border-1 border-green m-2">
+                    <h2 class=" text-center text-green">{{ value.name }}</h2>
+    
+                </div>
+
+            </section>
+
+        </div>
+    
+
+<!-- {{ character }} -->
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+onMounted(() => {
+    getCharacter()
+})
+
+const character = ref(null)
+const getCharacter = async()=>{
+    await axios.get('https://rickandmortyapi.com/api/character')
+        .then((resolve)=>{
+            console.log(resolve.data.results)
+            character.value = resolve.data.results
+        }).catch((eror)=>{
+            console.log(eror);
+        })
+}
+
+
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'green':'#008000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
